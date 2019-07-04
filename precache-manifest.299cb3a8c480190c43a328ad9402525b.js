cae1695176b8cabcbe1653259ec65eea28c83f71
Sofiqul Islam <schoolwab@gmail.com>
2019-07-04 18:45:08 +0600

    Updates

---
+++ precache-manifest.299cb3a8c480190c43a328ad9402525b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d3e1e94ea14afcdb0ec65f3d757cac",
+    "url": "/practiceReact/index.html"
+  },
+  {
+    "revision": "73451781995e82b9d3e7",
+    "url": "/practiceReact/static/css/2.c075f904.chunk.css"
+  },
+  {
+    "revision": "5acb4fffc8d2b8141134",
+    "url": "/practiceReact/static/css/main.17d2781f.chunk.css"
+  },
+  {
+    "revision": "73451781995e82b9d3e7",
+    "url": "/practiceReact/static/js/2.2d656f3b.chunk.js"
+  },
+  {
+    "revision": "5acb4fffc8d2b8141134",
+    "url": "/practiceReact/static/js/main.0f272580.chunk.js"
+  },
+  {
+    "revision": "2263dd8359ae78f0972b",
+    "url": "/practiceReact/static/js/runtime~main.68d24e60.js"
+  },
+  {
+    "revision": "8234b419a1ab91354c7e3e5c3cd8ec9d",
+    "url": "/practiceReact/static/media/bg1.8234b419.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/practiceReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/practiceReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/practiceReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/practiceReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/practiceReact/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "32a31e4ee725d75f1cab67c98122cf73",
+    "url": "/practiceReact/static/media/logo.32a31e4e.png"
+  }
+]);
